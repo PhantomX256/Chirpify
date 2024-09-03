f2cd3d548a2736b24ac6ffba4729cad4f1487cfb
PhantomX256 <vedverma2004@gmail.com>
2024-09-03 21:40:25 +0530

    Added necessary input fields

--- client/src/components/forms/PostForm.tsx
+++ client/src/components/forms/PostForm.tsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import Input from '../shared/Input';
 import TextArea from '../shared/TextArea';
 import FileUploader from '../shared/FileUploader';
+import Button from '../shared/Button';
 
 const PostForm = () => {
   const [title, setTitle] = useState('');
   const [caption, setCaption] = useState('');
+  const [tags, setTags] = useState('');
 
   return (
     <div>
@@ -13,6 +15,11 @@ const PostForm = () => {
         <Input type='text' label='Title' name='title' value={title} setState={setTitle} placeholder='Your awesome title' />
         <TextArea label='Caption' placeholder='Your awesome caption' value={caption} setState={setCaption} />
         <FileUploader label='Add Meme' />
+        <Input type='text' placeholder='Funny, Dark, Hilarious' label='Tags (separated by comma "," )' name='tags' value={tags} setState={setTags} />
+        <span style={{ display: 'flex', alignSelf: 'flex-end', gap: '15px' }}>
+          <Button className='post-cancel-button' type='button'>Cancel</Button>
+          <Button className='post-submit-button' type='submit'>Submit</Button>
+        </span>
       </form>
     </div>
   )
